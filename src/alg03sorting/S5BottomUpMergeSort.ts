72fe2b0928794891973cfa0ad7a165c9f5567a2a
deemson <deemson.k@gmail.com>
2019-04-20 00:03:55 +0300

    Renamings in bottom-up merge sort

--- src/alg03sorting/S5BottomUpMergeSort.ts
+++ src/alg03sorting/S5BottomUpMergeSort.ts
@@ -8,10 +8,10 @@ export class S5BottomUpMergeSort<T> extends S4MergeSort<T> implements Sort<T> {
     sort(array: T[]): void {
         const auxArray = Array<T>(array.length);
         for (let size = 1; size < array.length; size = 2 * size) {
-            for (let lowIndex = 0; lowIndex < array.length - size; lowIndex += 2 * size) {
-                const midIndex = lowIndex + size - 1;
-                const highIndex = Math.min(lowIndex + 2 * size - 1, array.length - 1);
-                this.merge(array, auxArray, lowIndex, midIndex, highIndex);
+            for (let leftThreshold = 0; leftThreshold < array.length - size; leftThreshold += 2 * size) {
+                const midThreshold = leftThreshold + size - 1;
+                const rightThreshold = Math.min(leftThreshold + 2 * size - 1, array.length - 1);
+                this.merge(array, auxArray, leftThreshold, midThreshold, rightThreshold);
             }
         }
     }
