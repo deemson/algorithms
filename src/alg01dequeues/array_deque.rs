f2f6d92d89620ac5da57bf5692eefe9046440710
Kurkin <deemson.k@gmail.com>
2023-02-17 17:03:47 +0200

    added method item_size

--- src/alg01dequeues/array_deque.rs
+++ src/alg01dequeues/array_deque.rs
@@ -16,11 +16,15 @@ impl<T> ArrayDeque<T> {
             length: 0,
         }
     }
+
+    fn item_size(&self) -> usize {
+        std::mem::size_of::<T>()
+    }
 }
 
 impl<T> Deque<T> for ArrayDeque<T> {
     fn add_last(&mut self, item: T) {
-        assert_ne!(std::mem::size_of::<T>(), 0, "No zero sized types");
+        assert_ne!(self.item_size(), 0, "No zero sized types");
         if self.capacity == 0 {
             let layout = alloc::Layout::array::<T>(4).expect("Could not obtain alloc layout");
             let pointer = unsafe { alloc::alloc(layout) } as *mut T;
@@ -30,18 +34,18 @@ impl<T> Deque<T> for ArrayDeque<T> {
             self.capacity = 4;
             self.length = 1;
         } else if self.length < self.capacity {
-            let offset = self.length.checked_mul(std::mem::size_of::<T>()).expect("Cannot reach memory location");
+            let offset = self.length.checked_mul(self.item_size()).expect("Cannot reach memory location");
             assert!(offset < isize::MAX as usize, "Wrapped around isize");
             unsafe { self.pointer.as_ptr().add(self.length).write(item) }
             self.length += 1;
         } else {
             let new_capacity = self.capacity.checked_mul(2).expect("capacity wrapped around usize");
             let align = std::mem::align_of::<T>();
-            let size = std::mem::size_of::<T>() * self.capacity;
+            let size = self.item_size() * self.capacity;
             size.checked_add(size % align).expect("can't allocate");
             let pointer = unsafe {
                 let layout = alloc::Layout::from_size_align_unchecked(size, align);
-                let new_size = std::mem::size_of::<T>() * new_capacity;
+                let new_size = self.item_size() * new_capacity;
                 let pointer = alloc::realloc(self.pointer.as_ptr() as *mut u8, layout, new_size);
                 let pointer = NonNull::new(pointer as *mut T).expect("can't reallocate");
                 pointer.as_ptr().add(self.length).write(item);
