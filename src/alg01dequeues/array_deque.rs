9022275e450b47fbbe56fa19677356f5279ec089
Kurkin <deemson.k@gmail.com>
2023-02-20 11:30:59 +0200

    array deque refactored a bit

--- src/alg01dequeues/array_deque.rs
+++ src/alg01dequeues/array_deque.rs
@@ -2,21 +2,23 @@ use std::alloc;
 use std::ptr::NonNull;
 use super::deque::Deque;
 
+const MIN_NON_ZERO_CAPACITY: usize = 4;
+
 /// `ArrayDeque<T>` uses chunks of contiguous manually allocated memory for `Deque<T>`
 /// implementation. They are treated as a substitution for arrays and serve a similar purpose.
 /// `ArrayDeque<T>` re-allocates memory as necessary, imitating array grow and shrink functionality.
 pub struct ArrayDeque<T> {
-    pointer: NonNull<T>,
+    non_null_ptr: NonNull<T>,
     capacity: usize,
-    length: usize,
+    len: usize,
 }
 
 impl<T> ArrayDeque<T> {
     pub fn new() -> Self {
         ArrayDeque {
-            pointer: NonNull::dangling(),
+            non_null_ptr: NonNull::dangling(),
             capacity: 0,
-            length: 0,
+            len: 0,
         }
     }
 
@@ -27,56 +29,61 @@ impl<T> ArrayDeque<T> {
     fn item_align(&self) -> usize {
         std::mem::align_of::<T>()
     }
+
+    fn grow_from_zero(&mut self) {
+        let layout = alloc::Layout::array::<T>(MIN_NON_ZERO_CAPACITY).expect("initial ArrayDeque memory layout exceeds isize::MAX");
+        let ptr = unsafe { alloc::alloc(layout) } as *mut T;
+        self.non_null_ptr = NonNull::new(ptr).expect("pointer to initial ArrayDeque allocated memory is null");
+        self.capacity = MIN_NON_ZERO_CAPACITY;
+    }
+
+    fn grow_if_required(&mut self) {
+        if self.capacity == 0 {
+            self.grow_from_zero();
+            return;
+        }
+        if self.len < self.capacity {
+            return;
+        }
+        let new_capacity = self.capacity.checked_mul(2).expect("ArrayDeque capacity exceeds usize::MAX");
+        let size = self.item_size() * self.capacity;
+        let new_size = self.item_size() * new_capacity;
+        let align = self.item_align();
+        // Adding the modulo remainder to size will make it properly aligned for capacity of aligned items
+        // this is one of the safety requirements for creating memory layout below
+        size.checked_add(size % align).expect("ArrayDeque aligned size exceeds usize::MAX");
+        // another safety requirement for creating memory layout below
+        assert!(size < isize::MAX as usize, "ArrayDeque aligned size exceeds isize::MAX");
+        self.non_null_ptr = unsafe {
+            let layout = alloc::Layout::from_size_align_unchecked(size, align);
+            let ptr = alloc::realloc(self.non_null_ptr.as_ptr() as *mut u8, layout, new_size);
+            NonNull::new(ptr as *mut T).expect("pointer to ArrayDeque re-allocated memory is null")
+        };
+        self.capacity = new_capacity;
+    }
 }
 
 impl<T> Deque<T> for ArrayDeque<T> {
     fn add_last(&mut self, item: T) {
-        assert_ne!(self.item_size(), 0, "No zero sized types");
-        if self.capacity == 0 {
-            let layout = alloc::Layout::array::<T>(4).expect("Could not obtain alloc layout");
-            let pointer = unsafe { alloc::alloc(layout) } as *mut T;
-            let pointer = NonNull::new(pointer).expect("Could not allocate pointer");
-            unsafe { pointer.as_ptr().write(item) }
-            self.pointer = pointer;
-            self.capacity = 4;
-            self.length = 1;
-        } else if self.length < self.capacity {
-            let offset = self.length.checked_mul(self.item_size()).expect("Cannot reach memory location");
-            assert!(offset < isize::MAX as usize, "Wrapped around isize");
-            unsafe { self.pointer.as_ptr().add(self.length).write(item) }
-            self.length += 1;
-        } else {
-            let new_capacity = self.capacity.checked_mul(2).expect("capacity wrapped around usize");
-            let align = self.item_align();
-            let size = self.item_size() * self.capacity;
-            size.checked_add(size % align).expect("can't allocate");
-            let pointer = unsafe {
-                let layout = alloc::Layout::from_size_align_unchecked(size, align);
-                let new_size = self.item_size() * new_capacity;
-                let pointer = alloc::realloc(self.pointer.as_ptr() as *mut u8, layout, new_size);
-                let pointer = NonNull::new(pointer as *mut T).expect("can't reallocate");
-                pointer.as_ptr().add(self.length).write(item);
-                pointer
-            };
-            self.pointer = pointer;
-            self.capacity = new_capacity;
-            self.length += 1;
-        }
+        assert_ne!(self.item_size(), 0, "ArrayDeque cannot allocate memory for zero-sized items");
+        self.grow_if_required();
+        unsafe { self.non_null_ptr.as_ptr().add(self.len).write(item) };
+        self.len += 1;
     }
 
     fn as_slice(&self) -> &[T] {
-        unsafe { std::slice::from_raw_parts(self.pointer.as_ptr(), self.length) }
+        unsafe { std::slice::from_raw_parts(self.non_null_ptr.as_ptr(), self.len) }
     }
 }
 
 impl<T> Drop for ArrayDeque<T> {
     fn drop(&mut self) {
         if self.capacity == 0 {
-            return
+            return;
         }
-        let pointer = self.pointer.as_ptr();
+        let pointer = self.non_null_ptr.as_ptr();
         unsafe {
-            let raw_slice_pointer = std::slice::from_raw_parts_mut(pointer, self.length);
+            let raw_slice_pointer = std::slice::from_raw_parts_mut(pointer, self.len);
             let layout = alloc::Layout::from_size_align_unchecked(
                 self.item_size() * self.capacity,
                 self.item_align(),
